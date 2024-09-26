a4baa8d5482c96f47ce4bc08a51556e7d992df5b
AnthonyCh05 <anthonychangotasig.clearminds@gmail.com>
2024-09-26 16:46:15 -0500

    add module dbz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: "../styles.css"
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   public title: string = 'Hola Mundo';
-
-  ngOnInit(): void {
-    initFlowbite();
-  }
 
 }
